e44bbe8c1fe99351d0f696a73918e7d5b1d8886f
lyjguy <lyjguy@naver.com>
2022-06-01 14:37:58 +0900

    kotlin-spring project init

---
+++ src/main/kotlin/com/lyjguy/kotlinspring/KotlinSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.lyjguy.kotlinspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/lyjguy/kotlinspring/KotlinSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lyjguy.kotlinspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
