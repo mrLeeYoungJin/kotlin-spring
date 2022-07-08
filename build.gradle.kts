573ca4844d7362fb9b1062777e9ed3c732395868
lyjguy <lyjguy@naver.com>
2022-07-09 01:03:32 +0900

    mockk spring rest docs dependencies 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
+import org.asciidoctor.gradle.jvm.AsciidoctorTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     id("org.springframework.boot") version "2.7.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
     kotlin("plugin.jpa") version "1.6.21"
+    kotlin("kapt") version "1.7.0"
 }
 
 group = "com.lyjguy"
@@ -18,6 +22,8 @@ configurations {
     }
 }
 
+val snippetsDir by extra { file("build/generated-snippets") }
+
 repositories {
     mavenCentral()
 }
@@ -28,10 +34,16 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    //runtimeOnly("com.h2database:h2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    runtimeOnly("com.h2database:h2")
     runtimeOnly("mysql:mysql-connector-java")
+
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.mockk:mockk:1.12.4")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
 
 tasks.withType<KotlinCompile> {
@@ -43,4 +55,21 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+val test = tasks.withType<Test> {
+    useJUnitPlatform()
+    outputs.dir(snippetsDir)
+}
+
+val asciidoctor = tasks.withType<AsciidoctorTask> {
+    inputs.dir(snippetsDir)
+    dependsOn(test)
+}
+
+tasks.withType<BootJar> {
+    dependsOn(asciidoctor)
+    from("$snippetsDir/html5") {
+        into("static/docs")
+    }
 }
