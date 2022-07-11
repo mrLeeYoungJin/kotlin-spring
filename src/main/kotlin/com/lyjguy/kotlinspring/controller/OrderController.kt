19a9784d94d5a590994acecccc1c3b3fa0fb4378
lyjguy <lyjguy@naver.com>
2022-07-11 13:42:37 +0900

    OrderController 수정

--- src/main/kotlin/com/lyjguy/kotlinspring/controller/OrderController.kt
+++ src/main/kotlin/com/lyjguy/kotlinspring/controller/OrderController.kt
@@ -3,23 +3,20 @@ package com.lyjguy.kotlinspring.controller
 import com.lyjguy.kotlinspring.model.dto.ReqOrderDto
 import com.lyjguy.kotlinspring.model.vo.OrderVO
 import com.lyjguy.kotlinspring.service.OrderService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/api/v1/orders")
 class OrderController(
     val orderService: OrderService,
 ) {
 
-    @GetMapping("/orders/{orderId}")
-    fun getOrder(@PathVariable("orderId") orderId : Long): OrderVO {
+    @GetMapping("/{id}")
+    fun getOrder(@PathVariable("id") orderId : Long): OrderVO {
         return orderService.getOrder(orderId = orderId)
     }
 
-    @PostMapping("/orders")
+    @PostMapping
     fun savePost(@RequestBody reqOrderDto: ReqOrderDto) {
         orderService.save(reqOrderDto)
     }
